386d086efe5a9c1cab7451d43d215834b377e4e0
Joao Augusto <augustoj311@gmail.com>
2022-03-16 22:58:33 -0300

    fix: fixing babel tests

--- frontend/cypress/integration/index.spec.js
+++ frontend/cypress/integration/index.spec.js
@@ -1,7 +1,7 @@
 /// <reference types="cypress" />
 
-describe('Navigation', () => {
-  it('should navigate to the about page', () => {
+describe('Index', () => {
+  it('Should show the index page', () => {
     cy.visit('http://localhost:3000/')
 
     cy.contains('Welcome to Next.js!')
