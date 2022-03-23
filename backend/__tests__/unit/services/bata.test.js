8a9bf11d55e4d77e67fc6b376282989522f4e35a
Joao Augusto <augustoj311@gmail.com>
2022-03-22 22:54:17 -0300

    fix: fixing tests

--- backend/__tests__/unit/services/bata.test.js
+++ backend/__tests__/unit/services/bata.test.js
@@ -1,10 +1,14 @@
 const batata = require('../../../src/services/saveForMeService/batata')
 
-jest.setTimeout(100000)
+jest.mock('../../../src/config/prisma', () => ({
+  linkContent: {
+    create: jest.fn().mockReturnValue({ id: '' }),
+  },
+}))
 
 describe('[logger] Test case', () => {
   it('Should log all severity and beautify', async () => {
     const response = await batata()
-    expect(response).toBe(1)
+    expect(response).toStrictEqual({ id: '' })
   })
 })
