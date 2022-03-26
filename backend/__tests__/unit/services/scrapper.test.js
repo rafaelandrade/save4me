e2fae157677bdf4348f4e1bca0b3fb03fc2f3ab9
Joao Augusto <augustoj311@gmail.com>
2022-03-26 20:05:00 -0300

    fix: fixing tests

--- backend/__tests__/unit/services/scrapper.test.js
+++ backend/__tests__/unit/services/scrapper.test.js
@@ -7,8 +7,7 @@ describe('[scrapper] Test case', () => {
     const metadata = await scrapper(site)
 
     expect(metadata).toEqual({
-      description:
-        'Tiny, fast, and elegant implementation of core jQuery designed specifically for the server. Latest version: 1.0.0-rc.10, last published: 9 months ago. Start using cheerio in your project by running `npm i cheerio`. There are 13420 other projects in the npm registry using cheerio.',
+      description: expect.any(String),
       image: 'https://static.npmjs.com/338e4905a2684ca96e08c7780fc68412.png',
       title: 'cheerio - npmGitDownloads',
     })
