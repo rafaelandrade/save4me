81c4cad7de0795cce53bd5788f7ce9b36930f172
Joao Augusto <augustoj311@gmail.com>
2022-03-27 17:33:53 -0300

    feat: adding logo

--- frontend/public/background.js
+++
@@ -1,9 +0,0 @@
-chrome.commands.onCommand.addListener((command) => {
-  console.log(`Command "${command}" triggered`)
-})
-
-chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
-  const url = tabs[0].url
-
-  console.log(url)
-})
