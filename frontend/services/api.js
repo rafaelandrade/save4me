8b9c7e90e17db51306ebe5af23137deaf5c90e3f
Joao Augusto <augustoj311@gmail.com>
2022-03-30 16:47:48 -0300

    feat: ading feature to inject envs

--- frontend/services/api.js
+++ frontend/services/api.js
@@ -1,14 +1,9 @@
 import axios from 'axios'
 import * as Sentry from '@sentry/nextjs'
 import './sentry'
+import axiosConfig from '../config/api.json'
 
-const api = axios.create({
-  baseURL: 'https://save4me-backend.herokuapp.com/',
-  headers: {
-    authorization:
-      '0449f26a266df07cf3bb8afa5c9a19934985cc0336ca13f77be88a75a804dc280953bcae4f566b35970d0d1f3329af712f00b50cfa7a5ac156bbb409f16b4829',
-  },
-})
+const api = axios.create(axiosConfig)
 
 const fetchBackend = async ({ email, data, service = 'get' }) => {
   try {
