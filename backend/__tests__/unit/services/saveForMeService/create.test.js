9c65b9be3b7b459068324a35f8694f596debd8ef
Joao Augusto <augustoj311@gmail.com>
2022-03-30 14:15:41 -0300

    fix: fixing tests

--- backend/__tests__/unit/services/saveForMeService/create.test.js
+++ backend/__tests__/unit/services/saveForMeService/create.test.js
@@ -1,10 +1,19 @@
 const create = require('../../../../src/services/saveForMeService/create')
 const prisma = require('../../../../src/config/prisma')
+const update = require('../../../../src/services/saveForMeService/update')
+const scrapper = require('../../../../src/services/scrapper')
+
+jest.mock('../../../../src/services/saveForMeService/update')
+jest.mock('../../../../src/services/scrapper')
 
 describe('[create] Test case', () => {
   it('Should called prisma and should be called with same parameters', async () => {
-    const data = { keywords: ['facebook', 'discord', 'medium'], link: 'link' }
+    const data = { keywords: ['facebook', 'discord', 'medium'], link: 'link', title: 'title' }
 
+    // @ts-ignore
+    update.mockImplementation(() => {})
+    // @ts-ignore
+    scrapper.mockImplementation(() => ({ image: 'image' }))
     prisma.linkContent.create = jest.fn().mockReturnValue('')
 
     // @ts-ignore
@@ -13,7 +22,16 @@ describe('[create] Test case', () => {
     expect(response).not.toBeUndefined()
     expect(prisma.linkContent.create).toBeCalled()
     expect(prisma.linkContent.create).toBeCalledWith({
-      data: { data: { id: 1, link: 'link', keywords: ['facebook', 'discord', 'medium'] }, email: 'email@teste.com' },
+      data: {
+        data: {
+          id: expect.any(String),
+          link: 'link',
+          title: 'title',
+          icon: 'image',
+          keywords: ['facebook', 'discord', 'medium'],
+        },
+        email: 'email@teste.com',
+      },
     })
   })
 })
