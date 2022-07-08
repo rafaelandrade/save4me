734a0347d25114037fb97371f096a055e2ea80a2
Joao Augusto <augustoj311@gmail.com>
2022-07-07 21:06:29 -0300

    feat: adding firefox and cli to build both

---
+++ cli/build/src/cli.js
@@ -0,0 +1,149 @@
+import { exec } from 'child_process'
+import path from 'path'
+const { Observable } = require('rxjs')
+const arg = require('arg')
+const inquirer = require('inquirer')
+const fs = require('fs')
+const Listr = require('listr')
+
+function parseArgumentsIntoOptions(rawArgs) {
+  const args = arg(
+    {
+      '--chrome': Boolean,
+      '--firefox': Boolean,
+      '--zip': Boolean,
+    },
+    {
+      argv: rawArgs.slice(2),
+    }
+  )
+
+  return {
+    chrome: args['--chrome'],
+    firefox: args['--firefox'],
+    zip: args['--zip'] || null,
+  }
+}
+
+function getBrowser(options) {
+  if (options.chrome) return 'Chrome'
+
+  if (options.firefox) return 'Firefox'
+
+  return null
+}
+
+async function promptForMissingOptions(options) {
+  const questions = []
+
+  const browser = getBrowser(options)
+
+  if (!browser) {
+    questions.push({
+      type: 'list',
+      name: 'browser',
+      message: 'Which browser will you use?',
+      default: 'Chrome',
+      choices: ['Chrome', 'Firefox'],
+    })
+  }
+
+  if (options.zip === null) {
+    questions.push({
+      type: 'confirm',
+      name: 'zip',
+      message: 'Do you want to zip the file?',
+      default: false,
+    })
+  }
+
+  const answers = await inquirer.prompt(questions)
+
+  return {
+    zip: options.zip || answers.zip,
+    browser: browser || answers.browser,
+  }
+}
+
+function writingManifestJson(manifest) {
+  manifest.background = {
+    scripts: ['background.js'],
+  }
+  manifest.manifest_version = 2
+}
+
+function buildFrontend() {
+  return new Observable((observer) => {
+    exec('cd frontend && yarn build', (error) => {
+      if (error) observer.error()
+
+      observer.complete()
+    })
+  })
+}
+
+function zipFile() {
+  return new Observable((observer) => {
+    exec('cd frontend && yarn zip:firefox', (error) => {
+      if (error) observer.error()
+
+      observer.complete()
+    })
+  })
+}
+
+function deleteExistingManifest() {
+  try {
+    fs.unlinkSync(path.join(__dirname, '..', '..', '..', 'frontend/public/manifest.json'))
+
+    return undefined
+  } catch (error) {
+    return 'Skipping because file does not exist'
+  }
+}
+
+export async function cli(args) {
+  const options = parseArgumentsIntoOptions(args)
+
+  const { browser, zip } = await promptForMissingOptions(options)
+
+  const defaultManifest = fs.readFileSync(
+    path.join(__dirname, '..', '..', '..', 'frontend/public/manifest.default.json'),
+    'utf-8'
+  )
+
+  const manifest = JSON.parse(defaultManifest)
+
+  // @ts-ignore
+  const tasks = new Listr([
+    {
+      title: 'Writing in manifest.json',
+      task: () => writingManifestJson(manifest),
+      skip: () => (browser === 'Chrome' ? 'Skipping writing for chrome' : undefined),
+    },
+    {
+      title: 'Deleting existing manifest.json',
+      task: () => deleteExistingManifest(),
+      skip: () => deleteExistingManifest(),
+    },
+    {
+      title: 'Writing manifest.json',
+      task: () =>
+        fs.writeFileSync(
+          path.join(__dirname, '..', '..', '..', 'frontend/public/manifest.json'),
+          JSON.stringify(manifest, null, 2)
+        ),
+    },
+    {
+      title: 'Building',
+      task: buildFrontend,
+    },
+    {
+      title: 'Ziping',
+      task: zipFile,
+      skip: () => (!zip ? 'Pass --zip to zip file' : undefined),
+    },
+  ])
+
+  tasks.run()
+}
